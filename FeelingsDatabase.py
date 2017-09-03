088ebaebf057aed71bd20ec7f420f9a5dce4689b
Maxim Minchuk <maxim.11209@gmail.com>
2017-09-03 00:26:50 -0500

    Initial changing of words and editing emotions
    Clean up time

--- FeelingsDatabase.py
+++ FeelingsDatabase.py
@@ -13,47 +13,48 @@ happy = ["open", "happy", "alive", "good", "understandable", "great",
 "bright", "sunny", "blessed","merry", "reassured", "elated", "jubilant",
 "positive", "excited", "confident", "eager", "enthusiastic", "hopeful",
 "keen", "earnest", "intent", "anxious", "challenged", "inspired",
-"optimistic", "determined", "re-enforced", "won", "family", "including", "mother",
-"comprehend", "new"]
+"optimistic", "determined", "re-enforced", "won", "love", "loving", "considerate", "affectionate", "sensitive", "tender", "devoted",
+"attracted", "passionate", "admiration", "warm", "touched", "sympathy",
+"close", "loved", "comforted"]
+
 sad = ["sad", "tearful", "sorrowful", "pained", "grief", "anguish", "desolate", "desperate",
 "pessimistic", "unhappy", "lonely", "grieved", "mournful", "dismayed",
 "depressed", "lousy", "disappointed", "discouraged", "ashamed",
 "powerless", "diminished", "guilty", "dissatisfied", "miserable",
 "detestable", "repugnnt", "despicable", "disgusting", "abominable",
-"complicated", "terrible", "in despair", "sulky", "bad", "a sense of loss", "no"]
-mad = ["angry", "irritated", "enraged", "hostile", "insulting", "sore",
+"complicated", "terrible", "in despair", "sulky", "bad", "a sense of loss", "upset", "pessimistic"]
+
+angry = ["angry", "irritated", "enraged", "hostile", "insulting", "sore",
 "annoyed", "upset", "hateful", "unpleasant", "offensive", "bitter",
 "aggressive", "resentful", "inflamed", "provoked", "incensed",
 "infuriated", "cross", "worked up", "boiling", "fuming", "indignant",
 "away"]
-confusion = ["confused", "upset", "doubtful", "uncertain",
+
+disgusted = [] 
+
+surprised = ["confused", "doubtful", "uncertain",
 "indecisive", "perplexed", "embarrassed", "hesitant", "shy",
 "stupefied", "disillusioned", "unbelieving", "skeptical", "distrustful",
-"misgiving", "lost", "unsure", "uneasy", "pessimistic", "tense",
-"empty"]
+"misgiving", "lost", "unsure", "uneasy", , "tense"]
+
 neutral = ["indifferent", "insensitive", "dull", "nonchalant",
 "neutral", "reserved", "weary", "bored", "preoccupied", "cold",
 "disinterested", "lifeless", "maybe", "process", "different",
 "adapted", "adapt", "adapting"]
-others = ["I", "you", "we", "he", "she", "they", "all", "the", "can", "should",
+
+'''others = ["I", "you", "we", "he", "she", "they", "all", "the", "can", "should",
 "would", "can't", "cannot", "shouldn't", "should not", "my",
 "would not", "wouldn't", "of", "it's", "it", "is", "me", "where", "took", "taken", "will"
 , "live", "their", "going", "in", "one", "that", "two", "three", "four", "five"
 "six", "seven", "eight", "nine", "ten", "go", "to", "and", "every",
-"language", "green"]
-scared = ["afraid", "hurt", "fearful", "escape", "escaped", "fear",
+"language", "green"]'''
+
+fear = ["afraid", "hurt", "fearful", "escape", "escaped", "fear",
 "crushed", "terrified", "tormented", "suspicious", "deprived",
 "anxious", "pained", "alarmed", "tortured", "panic", "dejected",
 "nervous", "rejected", "scared", "injured", "worried", "offended",
-"frightened", "afflicted",  "timid", "ching", "shaky", "victimized",
+"frightened", "afflicted",  "timid", "shaky", "victimized",
 "restless", "heartbroken", "doubtful", "agonized", "threatened",
 "appalled", "cowardly", "humiliated", "quaking", "wronged", "menaced",
-"alienated", "wary", "war"]
-interested = ["interested", "concerned",
-"affected", "fascinated", "intrigued", "absorbed",  "inquisitive",
-"nosy",  "snoopy", "engrossed", "curious"]
-love = ["love", "loving", "considerate", "affectionate", "sensitive", "tender", "devoted",
-"attracted", "passionate", "admiration", "warm", "touched", "sympathy",
-"close", "loved", "comforted", "drawn" + "toward"]
-brave = ["strong", "impulsive", "free", "sure", "certain", "rebellious", "unique",
-"dynamic", "tenacious", "hardy", "secure", "bold", "brave", "daring"]
+"alienated", "wary", "war", "submissive", "insecure", "disrespected", "inadequate",
+"insignificant", "worthless", "worried" "overwhelmed", "frightened", "terrified"]
