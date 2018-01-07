b49e1599aa549ad82cc9fbb0b7bce5fc4f70b92f
Maxim Minchuk <maxim.11209@gmail.com>
2018-01-06 23:59:20 -0500

    Test Run

--- Empath 0.02/Empath.py
+++ Empath 0.02/Empath.py
@@ -4,7 +4,15 @@ dataBase = importDatabase.get()
 #print("Database imported successfully!")
 #print(dataBase)
 
+import Counter 
+
 ### Open Test File
 from tkinter.filedialog import askopenfilename
 testFilename = askopenfilename()
+Counter.allWords(testFilename)
+Counter.emotion(testFilename, "happy", dataBase)
+Counter.nonEmotion(testFilename, dataBase)
 testFilename.close()
+
+
+
